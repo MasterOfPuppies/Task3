edeee76575b2113ba21a69f05d96d16d7409ab73
MasterOfPuppies <EgorZhuravlev@coherentsolutions.com>
2016-07-08 12:40:12 +0300

    Task 3 finalized

--- src/test/java/Class1.java
+++ src/test/java/Class1.java
@@ -1,33 +1,27 @@
 import org.junit.FixMethodOrder;
 import org.junit.runners.MethodSorters;
-import org.junit.runners.Suite;
 import org.testng.annotations.*;
 
+import static java.lang.Integer.parseInt;
 import static org.testng.Assert.assertEquals;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * Created by EgorZhuravlev on 7/7/2016.
  */
 
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+//@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class Class1 {
 
     private static Mathematics m = new Mathematics();
 
     private static void println(String string) {
         System.out.println(string);
-    }
-
-
-    @DataProvider(name = "DataForAdd")
-    public static Object[][] paramDataForAdd() {
-        return new Object[][]{
-                {0, 4, 4},
-                {3, 5, 8},
-                {9, 6, 15}
-        };
     }
 
     @DataProvider(name = "DataForDeduct")
@@ -48,43 +42,69 @@ public class Class1 {
         };
     }
 
-
-    @BeforeTest
+    @BeforeTest(alwaysRun = true)
     public void beforeTests() {
         //code
     }
 
-    @AfterTest
+    @AfterTest(alwaysRun = true)
     public void afterTests() {
         //code
     }
 
-    @BeforeClass
+    @BeforeClass(alwaysRun = true)
     public static void testsStarted() {
         m = new Mathematics();
         println("All tests have started");
     }
 
-    @AfterClass
+    @AfterClass(alwaysRun = true)
     public static void testsFinished() throws IOException {
         println("All tests have finished");
         m = null;
     }
 
-    @BeforeMethod
+    @BeforeMethod(alwaysRun = true)
     public void testStarted() {
         m.setResult(0);
         println("Before Test");
     }
 
-    @AfterMethod
+    @AfterMethod(alwaysRun = true)
     public void testFinished() {
 
         println("After Test");
     }
 
+    @DataProvider(name = "fileData")
+    public Object[][] testData() throws IOException {
+        int numLines = 0;
+        int i = 0;
+        int j = 0;
+        File file = new File("file.txt");
 
-    @Test(dataProvider = "DataForAdd")
+        BufferedReader br = new BufferedReader(new FileReader(file.getAbsoluteFile()));
+        while ((br.readLine()) != null)
+        {
+            numLines++;
+        }
+        br.close();
+
+        Object[][] testData = new Object[numLines][3];
+        Scanner sc = new Scanner(file);
+        while (sc.hasNextInt()) {
+            testData[j][i] = sc.nextInt();
+            i++;
+
+            if ((i % 3) == 0) {
+                j++;
+                i = 0;
+            }
+        }
+        return testData;
+    }
+
+    @Test(dataProvider = "fileData", groups = "fast")
     public void add_Test(int x, int y, int z) {
 
         m.add(x, y);
@@ -92,7 +112,7 @@ public class Class1 {
         assertEquals(z, m.getResult());
     }
 
-    @Test(timeOut = 10, dataProvider = "DataForDeduct", dependsOnMethods = {"divideExceptionTest"})
+    @Test(timeOut = 10, dataProvider = "DataForDeduct", dependsOnMethods = {"divideExceptionTest"}, groups = "fast")
     public void deduct_Test(int x, int y, int z) {
 
         m.deduct(x, y);
@@ -100,8 +120,7 @@ public class Class1 {
         assertEquals(z, m.getResult());
     }
 
-
-    @Test
+    @Test(groups = "fast")
     @Parameters({"xmlParamsX", "xmlParamsY", "xmlParamsZ"})
     public void multiply_Test(int x, int y, int z) {
 
@@ -110,7 +129,7 @@ public class Class1 {
         assertEquals(z, m.getResult());
     }
 
-    @Test(dataProvider = "DataForDiv")
+    @Test(dataProvider = "DataForDiv", groups = {"smoke", "fast"})
     public void divide_Test(int x, int y, int z) {
 
         m.divide(x, y);
@@ -118,7 +137,7 @@ public class Class1 {
         assertEquals(z, m.getResult());
     }
 
-    @Test(expectedExceptions = ArithmeticException.class)
+    @Test(expectedExceptions = ArithmeticException.class, groups = "fast")
     public void divideExceptionTest() {
 
         m.divide((int) Math.random(), 0);
--- src/test/java/Class2.java
+++ src/test/java/Class2.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
  * Created by EgorZhuravlev on 7/7/2016.
  */
 public class Class2 {
-    @Test
+    @Test(groups = {"fast"})
     @Ignore
     public void emptyTest(){
         //code it sometime
