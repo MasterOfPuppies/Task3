36909f137e69a67ccc54aa5eb3f310c70ae62eed
MasterOfPuppies <EgorZhuravlev@coherentsolutions.com>
2016-07-12 15:45:21 +0300

    Task 3 finalized

--- src/test/java/Class1.java
+++ src/test/java/Class1.java
@@ -2,14 +2,14 @@ import org.junit.FixMethodOrder;
 import org.junit.runners.MethodSorters;
 import org.testng.annotations.*;
 
+
 import static java.lang.Integer.parseInt;
+
 import static org.testng.Assert.assertEquals;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Scanner;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by EgorZhuravlev on 7/7/2016.
@@ -76,40 +76,112 @@ public class Class1 {
         println("After Test");
     }
 
-    @DataProvider(name = "fileData")
-    public Object[][] testData() throws IOException {
-        int numLines = 0;
-        int i = 0;
-        int j = 0;
-        File file = new File("file.txt");
 
-        BufferedReader br = new BufferedReader(new FileReader(file.getAbsoluteFile()));
-        while ((br.readLine()) != null)
-        {
-            numLines++;
-        }
-        br.close();
 
-        Object[][] testData = new Object[numLines][3];
-        Scanner sc = new Scanner(file);
-        while (sc.hasNextInt()) {
-            testData[j][i] = sc.nextInt();
-            i++;
+    private static List<String> getStringsFromFile(File file) throws IOException {
+        List<String> info = new ArrayList<>();
 
-            if ((i % 3) == 0) {
-                j++;
-                i = 0;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file.getAbsoluteFile()));
+            String str = null;
+            while((str = br.readLine()) != null) {
+                info.add(str);
+
             }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
-        return testData;
+
+
+        return info;
     }
 
+
+    private static Object[][] getDataFromTextFile(File file, String divider) throws IOException {
+        List<String> text = getStringsFromFile(file);
+        int rowsCount = text.size();
+        int columnsCount = text.get(0).split(divider).length;
+        Object[][] objects = new Object[rowsCount][columnsCount];
+        for (int i = 0; i < text.size(); i++) {
+            objects[i] = text.get(i).split(divider);
+            }
+        return objects;
+    }
+
+//    public static void main(String[] args) {
+//        File file_txt = new File("file.txt");
+//        try {
+//            getDataFromTextFile(file_txt, " ");
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        System.out.print("ds");
+//    }
+
+    @DataProvider(name = "fileData")
+    public static Object[][] testData() throws IOException {
+        File file_txt = new File("file.txt");
+        return getDataFromTextFile(file_txt, " ");
+    }
+
+//    @DataProvider(name = "fileData")
+//    public Object[][] testData() throws IOException {
+//        int i = 0;
+//        int j = 0;
+//        int numLines = 0;
+//        File file = new File("file.txt");
+//        BufferedReader br = new BufferedReader(new FileReader(file.getAbsoluteFile()));
+//        while(br.readLine() != null) {
+//            numLines++;
+//        }
+//        br.close();
+//
+//        BufferedReader br2 = new BufferedReader(new FileReader(file.getAbsoluteFile()));
+//        Object[][] testData = new Object[numLines][3];
+//        String str;
+//        while ((str = br.readLine()) != null) {
+//            String[] s = str.split(" "); //15,19,21,dsfsdfsd
+//            s.length
+//            for(int k = 0; k < s.length; k++)
+//            {
+//                testData[i][j] = parseInt(s[k]);
+//                j++;
+//            }
+//            j = 0;
+//            i++;
+//
+//        }
+//        br2.close();
+
+//        Object[][] testData = new Object[numLines][];
+//        Scanner sc = new Scanner(file);
+//            while (sc.hasNext())
+//            {
+//                if(!sc.hasNextByte('\n'))
+//                {
+//                    testData[i][j] = sc.next();
+//                    j++;
+//                }
+//                else
+//                {
+//                    i++;
+//                }
+//            }
+
+
+//        return testData;
+//    }
+
     @Test(dataProvider = "fileData", groups = "fast")
-    public void add_Test(int x, int y, int z) {
+    public void add_Test(String x, String y, String z) {
+        int intX = Integer.parseInt(x);
+        int intY = Integer.parseInt(y);
+        int intZ = Integer.parseInt(z);
 
-        m.add(x, y);
+        m.add(intX, intY);
 
-        assertEquals(z, m.getResult());
+        assertEquals(intZ, m.getResult());
     }
 
     @Test(timeOut = 10, dataProvider = "DataForDeduct", dependsOnMethods = {"divideExceptionTest"}, groups = "fast")
